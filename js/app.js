bcdce27356c1a4ddeb032e053f72d7ae8e2b53dd
jimohsegun1 <99956721+jimohsegun1@users.noreply.github.com>
2022-07-02 14:52:47 +0100

    Add files via upload
    sailjavascript1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const row0 = document.getElementById("t0").innerHTML
+const row1 = document.getElementById("t1").innerHTML
+const row2 = document.getElementById("t2").innerHTML
+const row3 = document.getElementById("t3").innerHTML
+const row4 = document.getElementById("t4").innerHTML
+const row5 = document.getElementById("t5").innerHTML
+const row6 = document.getElementById("t6").innerHTML
+const row7 = document.getElementById("t7").innerHTML
+const row8 = document.getElementById("t8").innerHTML
+const row9 = document.getElementById("t9").innerHTML
+const row10 = document.getElementById("t10").innerHTML
+const row11 = document.getElementById("t11").innerHTML
+
+
+document.write("Heading : " + row0);
+document.write("\n");
+document.write("Sub heading : " + row1);
+document.write("\n");
+document.write("Details : " + row2)
+document.write("Details : " + row3)
+document.write("Details : " + row4)
+document.write("Details : " + row5)
+document.write("Details : " + row6)
+document.write("Details : " + row7)
+document.write("Details : " + row8)
+document.write("Details : " + row9)
+document.write("Details : " + row10)
+document.write("Details : " + row11)
