65482e66faa122b4fe0a64d5981d8cda0bfbf2ce
jimohsegun1 <99956721+jimohsegun1@users.noreply.github.com>
2022-07-03 22:08:54 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,101 @@
-const row0 = document.getElementById("t0").innerHTML
-const row1 = document.getElementById("t1").innerHTML
-const row2 = document.getElementById("t2").innerHTML
-const row3 = document.getElementById("t3").innerHTML
-const row4 = document.getElementById("t4").innerHTML
-const row5 = document.getElementById("t5").innerHTML
-const row6 = document.getElementById("t6").innerHTML
-const row7 = document.getElementById("t7").innerHTML
-const row8 = document.getElementById("t8").innerHTML
-const row9 = document.getElementById("t9").innerHTML
-const row10 = document.getElementById("t10").innerHTML
-const row11 = document.getElementById("t11").innerHTML
+const worker = ["Workers"]
+document.getElementById("t0").innerHTML = worker[0]
 
+const head = ["N0", "FirstName", "LastName", "Age", "Sex", "Position", "Date of Employment"]
+document.getElementById("head1").innerHTML = head[0]
+document.getElementById("head2").innerHTML = head[1]
+document.getElementById("head3").innerHTML = head[2]
+document.getElementById("head4").innerHTML = head[3]
+document.getElementById("head5").innerHTML = head[4]
+document.getElementById("head6").innerHTML = head[5]
+document.getElementById("head7").innerHTML = head[6]
 
-document.write("Heading : " + row0);
-document.write("\n");
-document.write("Sub heading : " + row1);
-document.write("\n");
-document.write("Details : " + row2)
-document.write("Details : " + row3)
-document.write("Details : " + row4)
-document.write("Details : " + row5)
-document.write("Details : " + row6)
-document.write("Details : " + row7)
-document.write("Details : " + row8)
-document.write("Details : " + row9)
-document.write("Details : " + row10)
-document.write("Details : " + row11)
+const row1 = ["1", "Thompson", "Anthony", "32", "Male", "Head of Finance Department", "17/07/2014"]
+document.getElementById("a1").innerHTML = row1[0]
+document.getElementById("a2").innerHTML = row1[1]
+document.getElementById("a3").innerHTML = row1[2]
+document.getElementById("a4").innerHTML = row1[3]
+document.getElementById("a5").innerHTML = row1[4]
+document.getElementById("a6").innerHTML = row1[5]
+document.getElementById("a7").innerHTML = row1[6]
+
+const row2 = ["2", "Johnson", "Mary", "23", "Female", "Software Engineer", "05/03/2019"]
+document.getElementById("b1").innerHTML = row2[0]
+document.getElementById("b2").innerHTML = row2[1]
+document.getElementById("b3").innerHTML = row2[2]
+document.getElementById("b4").innerHTML = row2[3]
+document.getElementById("b5").innerHTML = row2[4]
+document.getElementById("b6").innerHTML = row2[5]
+document.getElementById("b7").innerHTML = row2[6]
+
+const row3 = ["3", "Chisom", "Onyeka", "28", "Female", "Human Resource Manager", "21/06/2016"]
+document.getElementById("c1").innerHTML = row3[0]
+document.getElementById("c2").innerHTML = row3[1]
+document.getElementById("c3").innerHTML = row3[2]
+document.getElementById("c4").innerHTML = row3[3]
+document.getElementById("c5").innerHTML = row3[4]
+document.getElementById("c6").innerHTML = row3[5]
+document.getElementById("c7").innerHTML = row3[6]
+
+const row4 = ["4", "Jide", "Olabisi", "29", "Male", "Assistant Head (Software Department)", "05/06/2016"]
+document.getElementById("d1").innerHTML = row4[0]
+document.getElementById("d2").innerHTML = row4[1]
+document.getElementById("d3").innerHTML = row4[2]
+document.getElementById("d4").innerHTML = row4[3]
+document.getElementById("d5").innerHTML = row4[4]
+document.getElementById("d6").innerHTML = row4[5]
+document.getElementById("d7").innerHTML = row4[6]
+
+const row5 = ["5", "Williams", "Ajayi", "30", "Male", "Head of Software Department", "06/09/2015"]
+document.getElementById("e1").innerHTML = row5[0]
+document.getElementById("e2").innerHTML = row5[1]
+document.getElementById("e3").innerHTML = row5[2]
+document.getElementById("e4").innerHTML = row5[3]
+document.getElementById("e5").innerHTML = row5[4]
+document.getElementById("e6").innerHTML = row5[5]
+document.getElementById("e7").innerHTML = row5[6]
+
+const row6 = ["6", "Nancy", "Chukwuemeka", "27", "Female", "Secretary", "23/08/2016"]
+document.getElementById("f1").innerHTML = row6[0]
+document.getElementById("f2").innerHTML = row6[1]
+document.getElementById("f3").innerHTML = row6[2]
+document.getElementById("f4").innerHTML = row6[3]
+document.getElementById("f5").innerHTML = row6[4]
+document.getElementById("f6").innerHTML = row6[5]
+document.getElementById("f7").innerHTML = row6[6]
+
+const row7 = ["7", "Nicholas", "Uchemba", "26", "Male", "Software Engineer", "22/02/2020"]
+document.getElementById("g1").innerHTML = row7[0]
+document.getElementById("g2").innerHTML = row7[1]
+document.getElementById("g3").innerHTML = row7[2]
+document.getElementById("g4").innerHTML = row7[3]
+document.getElementById("g5").innerHTML = row7[4]
+document.getElementById("g6").innerHTML = row7[5]
+document.getElementById("g7").innerHTML = row7[6]
+
+const row8 = ["8", "Ahmed", "Olayinka", "24", "Male", "Graphics Designer", "15/08/2020"]
+document.getElementById("h1").innerHTML = row8[0]
+document.getElementById("h2").innerHTML = row8[1]
+document.getElementById("h3").innerHTML = row8[2]
+document.getElementById("h4").innerHTML = row8[3]
+document.getElementById("h5").innerHTML = row8[4]
+document.getElementById("h6").innerHTML = row8[5]
+document.getElementById("h7").innerHTML = row8[6]
+
+const row9 = ["9", "Halima", "Mohammed", "26", "Female", "Senior Manager", "30/05/2018"]
+document.getElementById("i1").innerHTML = row9[0]
+document.getElementById("i2").innerHTML = row9[1]
+document.getElementById("i3").innerHTML = row9[2]
+document.getElementById("i4").innerHTML = row9[3]
+document.getElementById("i5").innerHTML = row9[4]
+document.getElementById("i6").innerHTML = row9[5]
+document.getElementById("i7").innerHTML = row9[6]
+
+const row10 = ["10", "Richard", "Adeyemi", "41", "Male", "C.E.O", "	17/11/2020"]
+document.getElementById("j1").innerHTML = row10[0]
+document.getElementById("j2").innerHTML = row10[1]
+document.getElementById("j3").innerHTML = row10[2]
+document.getElementById("j4").innerHTML = row10[3]
+document.getElementById("j5").innerHTML = row10[4]
+document.getElementById("j6").innerHTML = row10[5]
+document.getElementById("j7").innerHTML = row10[6]
